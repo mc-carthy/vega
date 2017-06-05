99f8e1048f3d2d744e3c451cfbaf73d72aabaa20
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-05 09:58:12 +0100

    Decouple Vehicles controller from EF using Repository pattern

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -49,7 +49,7 @@ namespace vega.Controllers
             var vehicle = Mapper.Map<SaveVehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
 
-            context.Vehicles.Add(vehicle);
+            repository.Add(vehicle);
             await context.SaveChangesAsync();
 
             vehicle = await repository.GetVehicle(vehicle.Id);
@@ -87,14 +87,14 @@ namespace vega.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteVehicle(int id)
         {
-            var vehicle = await context.Vehicles.FindAsync(id);
+            var vehicle = await repository.GetVehicle(id, includeRelated: false);
 
             if (vehicle == null)
             {
                 return NotFound();
             }
 
-            context.Remove(vehicle);
+            repository.Remove(vehicle);
             await context.SaveChangesAsync();
 
             return Ok(id);

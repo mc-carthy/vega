b1cdab334080c97fb367c4a715c489b2712aea0e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-07 14:29:57 +0100

    Implement messy sorting

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -30,7 +30,7 @@ namespace vega.Persistence
             .SingleOrDefaultAsync(v => v.Id == id);
         }
 
-        public async Task<IEnumerable<Vehicle>> GetVehicles(Filter filter)
+        public async Task<IEnumerable<Vehicle>> GetVehicles(VehicleQuery queryObj)
         {         
             var query = context.Vehicles
             .Include(v => v.Model)
@@ -39,9 +39,26 @@ namespace vega.Persistence
                 .ThenInclude(vf => vf.Feature)
             .AsQueryable();
 
-            if (filter.MakeId.HasValue)
+            if (queryObj.MakeId.HasValue)
             {
-                query = query.Where(vega => vega.Model.MakeId == filter.MakeId.Value);
+                query = query.Where(vega => vega.Model.MakeId == queryObj.MakeId.Value);
+            }
+
+            if (queryObj.SortBy == "make")
+            {
+                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
+            }
+            if (queryObj.SortBy == "model")
+            {
+                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
+            }
+            if (queryObj.SortBy == "contactName")
+            {
+                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
+            }
+            if (queryObj.SortBy == "id")
+            {
+                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
             }
 
             return await query.ToListAsync();

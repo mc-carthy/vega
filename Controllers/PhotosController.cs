ddbf5789ebc7c1b0cb3a197253387a2bf313e5f6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-08 00:00:13 +0100

    Refactor using config file

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using vega.Controllers.Resources;
 using vega.Core;
 using vega.Core.Models;
@@ -15,23 +16,24 @@ namespace vega.Controllers
     [Route("/api/vehicles/{vehicleId}/photos")]
     public class PhotosController : Controller
     {
-        private readonly int MAX_BYTES = 10 * 1024 * 1024;
-        private readonly string[] ACCEPTED_FILE_TYPES = new [] { ".jpg", ".jpeg", ".png" };
         private readonly IHostingEnvironment host;
         private readonly IVehicleRepository repository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
+        private readonly PhotoSettings photoSettings;
         public PhotosController(
             IHostingEnvironment host,
             IVehicleRepository repository,
             IUnitOfWork unitOfWork,
-            IMapper mapper
+            IMapper mapper,
+            IOptionsSnapshot<PhotoSettings> options
         )
         {
             this.host = host;
             this.repository = repository;
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
+            this.photoSettings = options.Value;
         }
         
         [HttpPost]
@@ -51,11 +53,11 @@ namespace vega.Controllers
             {
                 return BadRequest("Empty File");
             }
-            if (file.Length > MAX_BYTES)
+            if (file.Length > photoSettings.MaxBytes)
             {
                 return BadRequest("Max file size (10Mb) exceeded");
             }
-            if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName)))
+            if (!photoSettings.AcceptedFileTypes.Any(s => s == Path.GetExtension(file.FileName)))
             {
                 return BadRequest("Invalid file type");
             }
---
+++ Core/Models/PhotoSettings.cs
@@ -0,0 +1,8 @@
+namespace vega.Core.Models
+{
+    public class PhotoSettings
+    {
+        public int MaxBytes { get; set; }
+        public string[] AcceptedFileTypes { get; set; }
+    }
+}

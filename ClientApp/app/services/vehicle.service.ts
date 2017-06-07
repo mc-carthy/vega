b728bfcb30d280fdeae34cc22c006f48b4bd4f1f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-07 19:15:57 +0100

    No longer hard-coding origin URL

--- ClientApp/app/services/vehicle.service.ts
+++ ClientApp/app/services/vehicle.service.ts
@@ -1,37 +1,36 @@
 import { Vehicle, SaveVehicle } from './../models/vehicle';
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class VehicleService {
-  private readonly absoluteUrl = 'http://localhost:5000';
   private readonly vehiclesEndpoint = '/api/vehicles/';
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, @Inject('ORIGIN_URL') private originUrl: string) { }
 
   getMakes()
   {
     // For some reason, absolute links must be used. Something to do with the location.origin provider in app.module.client.ts
-    return this.http.get(this.absoluteUrl + '/api/makes')
+    return this.http.get(this.originUrl + '/api/makes')
       .map(res => res.json());
   }
 
   getFeatures()
   {
-    return this.http.get(this.absoluteUrl + '/api/features')
+    return this.http.get(this.originUrl + '/api/features')
       .map(res => res.json());
   }
 
   create(vehicle)
   {
-    return this.http.post(this.absoluteUrl + this.vehiclesEndpoint, vehicle)
+    return this.http.post(this.originUrl + this.vehiclesEndpoint, vehicle)
       .map(res => res.json());
   }
 
   getVehicle(id)
   {
-    return this.http.get(this.absoluteUrl + this.vehiclesEndpoint + id)
+    return this.http.get(this.originUrl + this.vehiclesEndpoint + id)
       .map(res => res.json());
   }
 
@@ -44,7 +43,7 @@ export class VehicleService {
 
   getVehicles(filter)
   {
-    return this.http.get(this.absoluteUrl + this.vehiclesEndpoint + '?' + this.toQueryString(filter))
+    return this.http.get(this.originUrl + this.vehiclesEndpoint + '?' + this.toQueryString(filter))
       .map(res => res.json());
   }
 
@@ -65,13 +64,13 @@ export class VehicleService {
 
   update(vehicle: SaveVehicle)
   {
-    return this.http.put(this.absoluteUrl + this.vehiclesEndpoint + vehicle.id, vehicle)
+    return this.http.put(this.originUrl + this.vehiclesEndpoint + vehicle.id, vehicle)
       .map(res => res.json());
   }
 
   delete(vehicle)
   {
-    return this.http.delete(this.absoluteUrl + this.vehiclesEndpoint + vehicle.id)
+    return this.http.delete(this.originUrl + this.vehiclesEndpoint + vehicle.id)
       .map(res => res.json());
   }
 
